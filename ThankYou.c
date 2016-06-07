eb31b067575e8628a3958b36782ae7ab9ffeb78b
Landon Powell <landonjpowell@airmail.cc>
2016-06-07 01:22:13 -0400

    Update ThankYou.c

--- ThankYou.c
+++ ThankYou.c
@@ -1,9 +1,5 @@
 #include "stdio.h"
 main(int argc, char *argv[]) {
-    if (argc <= 1) {
-        printf("You're welcome.\n");
-    }
-    else {
-        printf("%s has been thanked.\n", argv[1]);
-    }
+    if (argc <= 1)  printf("You're welcome.\n"); 
+    else            printf("%s has been thanked.\n", argv[1]);
 }
